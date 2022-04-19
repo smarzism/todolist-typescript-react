28913fe810c74f980a60a0b740aaa6f28b6c550d
marzieh <marziehseyedmoosavi@gmail.com>
2022-04-19 11:28:25 +0430

    edit todo

--- todolist/src/components/SingleTodo.tsx
+++ todolist/src/components/SingleTodo.tsx
@@ -1,6 +1,7 @@
-import React from "react"
+import React, { useState, useRef, useEffect, useReducer } from "react"
 import { AiFillEdit, AiFillDelete, AiOutlineUndo } from "react-icons/ai"
 import { MdDone } from "react-icons/md"
+import { GrLinkNext } from "react-icons/gr"
 import { Todo } from "../model"
 import "./styles.css"
 interface singleTodoPropsType {
@@ -8,41 +9,78 @@ interface singleTodoPropsType {
   todos: Todo[]
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>
 }
+
 const SingleTodo = ({ todo, todos, setTodos }: singleTodoPropsType) => {
+  const [editMode, setEditMode] = useState<boolean>(false)
+  const [edit, setEdit] = useState<string>("")
+
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  // const [state, dispatch] = useReducer<React.ReducerWithoutAction>(
+  //   todoReducer,
+  //   todos
+  // )
+
+  const handleEdit = (e: React.FormEvent, id: number) => {
+    e.preventDefault()
+    edit && setTodos(todos.map((t) => (t.id === id ? { ...t, todo: edit } : t)))
+    setEditMode(false)
+  }
+  useEffect(() => {
+    inputRef.current?.focus()
+  }, [editMode])
+
   return (
     <form className="todos__single">
-      {todo.isDone ? (
-        <s className="todos__single--text"> {todo.todo}</s>
+      {editMode ? (
+        <>
+          <input
+            ref={inputRef}
+            defaultValue={edit}
+            onChange={(e) => setEdit(e.target.value)}
+            className="todos__single--text"
+          ></input>
+
+          <GrLinkNext onClick={(e) => handleEdit(e, todo.id)} />
+        </>
       ) : (
-        <span className="todos__single--text"> {todo.todo}</span>
-      )}
-      <div>
-        <span className="icon">
-          <AiFillEdit />
-          <AiFillDelete />
+        <>
           {todo.isDone ? (
-            <AiOutlineUndo
-              onClick={() =>
-                setTodos(
-                  todos.map((t) =>
-                    t.id === todo.id ? { ...t, isDone: false } : t
-                  )
-                )
-              }
-            />
+            <s className="todos__single--text"> {todo.todo}</s>
           ) : (
-            <MdDone
-              onClick={() =>
-                setTodos(
-                  todos.map((t) =>
-                    t.id === todo.id ? { ...t, isDone: true } : t
-                  )
-                )
-              }
-            />
+            <span className="todos__single--text"> {todo.todo}</span>
           )}
-        </span>
-      </div>
+          <div>
+            <span className="icon">
+              <AiFillEdit onClick={() => setEditMode(true)} />
+              <AiFillDelete
+                onClick={() => setTodos(todos.filter((t) => t.id !== todo.id))}
+              />
+              {todo.isDone ? (
+                <AiOutlineUndo
+                  onClick={() =>
+                    setTodos(
+                      todos.map((t) =>
+                        t.id === todo.id ? { ...t, isDone: false } : t
+                      )
+                    )
+                  }
+                />
+              ) : (
+                <MdDone
+                  onClick={() =>
+                    setTodos(
+                      todos.map((t) =>
+                        t.id === todo.id ? { ...t, isDone: true } : t
+                      )
+                    )
+                  }
+                />
+              )}
+            </span>
+          </div>
+        </>
+      )}
     </form>
   )
 }
--- todolist/src/model.ts
+++ todolist/src/model.ts
@@ -3,3 +3,22 @@ export interface Todo {
   todo: string
   isDone: boolean
 }
+// type theType = "add" | "remove" | "edit"
+
+// interface Action {
+//   type: theType
+//   payload: string | number
+// }
+
+// export function todoReducer(state: Todo[], action: Action) {
+//   switch (action.payload) {
+//     case "add":
+//       return state
+
+//     case "remove":
+//       return state
+
+//     case "edit":
+//       return state
+//   }
+// }
